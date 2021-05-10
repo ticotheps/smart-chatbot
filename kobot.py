bbdf8ce920e28bd15a7f5cf41b3b25ecd13a8008
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-05-10 16:19:47 -0400

    feat(kobot.py): enable the chat bot to gain context from previous conversations if possible

--- kobot.py
+++ kobot.py
@@ -28,3 +28,9 @@ def ask(question, chat_log=None):
     story = response['choices'][0]['text']
     
     return str(story)
+
+def append_interaction_to_chat_log(question, answer, chat_log=None):
+    if chat_log is None:
+        chat_log = session_prompt
+        
+    return f'{chat_log}{restart_sequence} {question}{start_sequence}{answer}'
